c5cb33cc013faf745cb6e4d5e7cf7127ef1dbdea
DragonflyStats <kobriendublin@gmail.com>
2013-10-27 00:54:41 +0100

    Create sigmoid.m

---
+++ Week-03/sigmoid.m
@@ -0,0 +1,19 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+denominator = 1 + exp(-1 * z);
+g = 1 ./ denominator;
+
+
+
+% =============================================================
+
+end
