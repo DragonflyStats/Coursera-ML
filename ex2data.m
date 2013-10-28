18c647b575abbf05674cc7a0821a1c878d9c87fc
DragonflyStats <kobriendublin@gmail.com>
2013-10-28 15:15:00 -0700

    Create ex2data.m

---
+++ ex2data.m
@@ -0,0 +1,11 @@
+%% Initialization
+clear all; close all; clc
+
+%% Load Data
+%  The first two columns contains the exam scores and the third column
+%  contains the label.
+
+data = load('ex2data1.txt');
+X = data(:, [1, 2]); y = data(:, 3);
+
+exp(X)
